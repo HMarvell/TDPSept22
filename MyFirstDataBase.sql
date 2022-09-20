b445ba09942977169fc50e16a9bba561b96741ef
unknown <harry.jack.marvell@gmail.com>
2022-09-20 16:04:50 +0100

    Add SQL

---
+++ MyFirstDataBase.sql
@@ -0,0 +1,9 @@
+Create database QA_Resturant;
+use QA_Resturant;
+create table Customers(
+Customer_ID INT Unique Not Null Auto_Increment,
+Customer_Name Varchar(100) Not Null,
+Phone_Number Char(11) Not Null,
+Primary Key(Customer_ID));
+Show Tables;
+Describe Customers;
