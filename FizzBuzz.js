32943854d10dfbeb9f101c988ffa944dc0caf79e
unknown <harry.jack.marvell@gmail.com>
2022-09-27 16:12:48 +0100

    Fixed FizzBuzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,17 +1,14 @@
 'use strict'
 
-for (let i =1; i <= 100; i+1)
+for (let i =1; i <= 100; i++)
 {
-    if((i % 3) == 0)
+    if((i % 3) == 0){
     console.log("Fizz");
-    else if ((i % 5) == 0)
+    } else if ((i % 5) == 0){
     console.log("Buzz");
-    else if 
-    (((i % 3) ==0) & ((i % 5) == 0))
+    } else if (((i % 3) ==0) & ((i % 5) == 0)){
     console.log("FizzBuzz");
-    else 
+    } else {
     console.log(i);
-    if (i >=100) stop
-
-
+    }
 }
