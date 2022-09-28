db5b11cf1aa2be1cb470477321e2614ac5fc4ebb
unknown <harry.jack.marvell@gmail.com>
2022-09-28 14:18:48 +0100

    add New Java Work Day2

---
+++ Function.js
@@ -0,0 +1,33 @@
+'use strict'
+
+function subCalc(num1, num2) {
+    return num1 - num2;
+}
+
+console.log(subCalc(10,12));
+
+const welcome = function(name,age,gender){
+    return console.log(`My name is ${name}, I am ${age} years old and ${gender}`);
+    }
+    welcome("Master Roshi",350,"A Hermit")
+
+   
+let num3 = (100)
+let num4 = (10)
+   function subCalc2(num3, num4) {
+        return num3 - num4;
+    }
+    function subCalc3(num3, num4) {
+        return (num3 + num4)
+    }
+    function subCalc4(num3, num4) {
+        return num3 / num4;
+    }
+    function subCalc5(num3, num4) {
+        return num3 * num4;
+    }
+    
+    console.log(subCalc2(num3,num4));
+    console.log(subCalc3(num3,num4));
+    console.log(subCalc4(num3,num4));
+    console.log(subCalc5(num3,num4));
